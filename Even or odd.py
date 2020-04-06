ce5aede3a9d233b0faef925b1de33b691a70ced2
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-04-06 13:16:19 -0700

    Add files via upload

---
+++ Even or odd.py
@@ -0,0 +1,16 @@
+print("IDENTIFY EVEN OR ODD NUMBER:\n")
+a = int(input("Enter number: "))
+if (a%2 == 0):
+    print("It's an even number!")
+else:
+    print("This number is odd!")
+
+print("\nIDENTIFY POSITIVE OR NEGATIVE NUMBER:\n")
+b = int(input("Enter number: "))
+if (b<0):
+     print("Neagative number")
+elif (a>0):
+    print("Positive number")
+else:
+    print("Zero")
+    
