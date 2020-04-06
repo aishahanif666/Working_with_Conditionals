2d80a62efa8b1f3240f0fdcc89eeb10331952c14
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-04-06 13:10:25 -0700

    Add files via upload

---
+++ Ticket Price.py
@@ -0,0 +1,9 @@
+a = int(input("What is your age? "))
+if (a>0) and (a<=3):
+    print("TICKET PRICE: 100")
+elif (a>3) and (a<=10):
+    print("TICKET PRICE: 150")
+elif (a>10) and (a<=20):
+    print("TICKET PRICE: 200")
+else:
+    print("TICKET PRICE: 250")
